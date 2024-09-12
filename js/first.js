9022f21ce9aac87a7e2899465b718e45a5a16b54
olha15 <hellardin@gmail.com>
2024-09-12 12:05:07 +0200

    plus functions

--- js/first.js
+++ js/first.js
@@ -17,3 +17,55 @@ const hero = {
 for(let key in hero){
     console.log(key, hero[key])
 }
+
+/* Створи об'єкт car з полем brand і методами showBrand (звичайна функція) та showBrandArrow (стрілкова функція).
+ Подивись, як працює this при виклику цих методів.
+ */
+
+
+let car = {
+    brand: "bmw",
+
+    showBrand: function  (){
+    console.log (this.brand)
+},
+
+     showBrandArrow: () => {
+    console.log(this.brand)
+},
+   
+};
+
+car.showBrandArrow()
+car.showBrand()
+
+/* Створи масив рядків, що представляє список товарів.
+ Використовуй стрілкову функцію з методом filter, щоб залишити тільки ті рядки, довжина яких перевищує 5 символів.
+Потім використай стрілкову функцію з методом map, щоб перетворити всі рядки у верхній регістр. */
+
+let goodsList = ['milk', 'eggs', 'cereals', 'bananas', 'pineapple',];
+
+let above5Symbols = goodsList.filter(str => str.length > 5);
+console.log(above5Symbols);
+
+let upperCase = goodsList.map(str => str.toUpperCase());
+console.log(upperCase);
+
+/* Застосуй стрілкові функції замість звичайних у наступному коді: */
+
+const words = ["apple", "banana", "grape", "kiwi", "strawberry"];
+
+/* Звичайна функція з filter
+const longWords = words.filter(function(word) {
+  return word.length > 5;
+}); */
+const longWords = words.filter(word => word.length >5);
+
+/* // Звичайна функція з map
+const uppercaseWords = words.map(function(word) {
+  return word.toUpperCase();
+}); */
+const uppercaseWords = words.map(word => word.toUpperCase());
+
+console.log(longWords);      // Виведе: ["banana", "strawberry"]
+console.log(uppercaseWords); // Виведе: ["APPLE", "BANANA", "GRAPE", "KIWI", "STRAWBERRY"]
